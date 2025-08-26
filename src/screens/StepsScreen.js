3277e62dd5aab8d17e76bb30bd852ecafe8d8fb4
EfraPayon777 <efraincortez777@gmail.com>
2025-08-26 08:27:48 -0600

    ejercico 2 guia 7

---
+++ src/screens/StepsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import StepScreen from './StepScreen';
+const StepsScreen = ({ steps, navigation }) => {
+    const [currentStep, setCurrentStep] = React.useState(0);
+    const handleNextStep = () => {
+        if (currentStep < steps.length - 1) {
+            setCurrentStep(currentStep + 1);
+        } else {
+            // Si estamos en el último paso, podemos navegar a otra pantalla oreiniciar los pasosnavigation.navigate('Inicio'); // Esto es solo un ejemplo, puedescambiarlo según tu flujo de la aplicación
+        }
+    };
+    return (
+        <StepScreen
+            step={steps[currentStep].step}
+            description={steps[currentStep].description}
+            onNextStep={handleNextStep}
+        />
+    );
+};
+export default StepsScreen;
